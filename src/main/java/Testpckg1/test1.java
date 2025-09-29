ffbb2e529ac63d775b4ea0a2170871d0ec45d4a5
reshmawani8-code <reshmawani8@gmail.com>
2025-09-29 22:55:47 +0530

    Initial commit

---
+++ src/main/java/Testpckg1/test1.java
@@ -0,0 +1,15 @@
+package Testpckg1;
+
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("Hellow");
+
+
+        int a= 23;
+        int b= 25;
+        int c= a + b ;
+        System.out.println(c);
+    }
+
+
+}
